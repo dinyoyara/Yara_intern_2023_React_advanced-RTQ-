05109f7aaee4ebdfee8c2a632e1c10931cf2fbfc
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-23 16:46:44 +0200

    Update store.

--- movies.app/src/store/index.js
+++ movies.app/src/store/index.js
@@ -1,14 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 
-import { userApi } from './apis/userApi';
+import { moviesApi } from './apis/moviesApi';
+import { favoritesReducer } from './slices/favoritesSlice';
+import { watchlistReducer } from './slices/watchlistSlice';
 
 export const store = configureStore({
     reducer: {
-        users: userApi.reducer
+        movies: moviesApi.reducer,
+        favorites: favoritesReducer,
+        watchlist: watchlistReducer
     },
     middleware: (getDefaultMiddleware) => {
-        return getDefaultMiddleware();
+        return getDefaultMiddleware().concat(moviesApi.middleware);
     }
 });
 

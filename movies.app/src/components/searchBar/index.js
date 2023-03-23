edb8008d753533f8de3b0d14b653caa84ab8002e
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-23 17:56:42 +0200

    Work on SearchBar functionality.

--- movies.app/src/components/searchBar/index.js
+++ movies.app/src/components/searchBar/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setQuery } from '../../store';
 
 export const SearchBar = () => {
-    const handleSearch = (e) => {
-        console.log();
+    const { inputValue, setInputValue } = useState();
+    const dispatch = useDispatch();
+
+    const handleInputChange = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleSearch = () => {
+        dispatch(setQuery(inputValue));
     };
 
     return (
         <form>
             <label htmlFor='search-input'>Title: </label>
-            <input id='search-input' type='text' />
-            <button type='button' on onClick={handleSearch}>
+            <input id='search-input' type='text' onChange={handleInputChange} />
+            <button type='button' onClick={handleSearch}>
                 Search
             </button>
         </form>

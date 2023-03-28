6f8160d1b732904bcf187937de9d4ebced81ff4e
dinyoyara <dinyo.dimitrov@yara.com>
2023-03-28 16:48:05 +0300

    Refactoring Body component.

--- movies.app/src/components/body/index.js
+++ movies.app/src/components/body/index.js
@@ -8,26 +8,21 @@ import MovieCollection from '../movieCollection';
 import { setActiveCollection } from '../../store';
 
 const Body = () => {
-    const search = useSelector((state) => {
-        return state.search;
-    });
-    const favorites = useSelector((state) => {
-        return state.favorites;
-    });
-    const watchlist = useSelector((state) => {
-        return state.watchlist;
-    });
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const navigateToFavorites = () => {
-        navigate('/favorites');
-        dispatch(setActiveCollection(process.env.REACT_APP_FAVORITES));
-    };
+    const search = useSelector((state) => state.search);
+    const favorites = useSelector((state) => state.favorites);
+    const watchlist = useSelector((state) => state.watchlist);
 
     const navigateToHome = () => {
         navigate('/');
         dispatch(setActiveCollection(''));
+    };
+
+    const navigateToFavorites = () => {
+        navigate('/favorites');
+        dispatch(setActiveCollection(process.env.REACT_APP_FAVORITES));
     };
 
     const navigateToWatchlist = () => {
@@ -37,7 +32,7 @@ const Body = () => {
 
     return (
         <>
-            <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+            <nav style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
                 <div style={{ display: 'flex', marginTop: '20px' }}>
                     <div
                         onClick={navigateToHome}
@@ -58,7 +53,7 @@ const Body = () => {
                         Watchlist
                     </div>
                 </div>
-            </div>
+            </nav>
             <Routes>
                 <Route
                     exact
